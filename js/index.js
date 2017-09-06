cd6397fb879cc5acf581e894fd59c6c60c594384
Salman Shakeel <shakeels22@gmail.com>
2017-09-05 22:30:26 -0400

    Fetch most popular movies and individual movie details

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var APIKey = "b37e4ca3ccc6c8c0a927bdcc9257084f";
+
+var movieDBBaseURL = "http://api.themoviedb.org/3/";     //http://api.themoviedb.org/3/movie/popular?api_key=b37e4ca3ccc6c8c0a927bdcc9257084f
+
+var movieRequestType = ["movie/", "find/"];         //[0] - collection, [1] - single movie
+var movieCollectionFlag = ["popular", "top_rated"];
+var movieCollectionRequestURL = movieDBBaseURL + movieRequestType[0] + movieCollectionFlag[0] + "?api_key=" + APIKey;
+
+var movieImageBaseURL = "http://image.tmdb.org/t/p/";
+var movieImageSize = "w185/";
+console.log(movieCollectionRequestURL);
+
+$.getJSON({
+url: movieCollectionRequestURL,
+  success: function(result) {
+    var movieList = result;
+    for(var i=0; i<10; i++){
+      var imgFileName = movieList.results[i].poster_path;
+      var movieID = movieList.results[i].id;
+      $('.movieList ul').append('<li><a href="movie-details.html?id=' + movieID + '"><img src="' + movieImageBaseURL + movieImageSize + imgFileName + '" alt="" /></a></li>');
+    }
+    console.log(movieList);
+  }
+});
---
+++ js/movie-details.js
@@ -0,0 +1,20 @@
+var URLParams = new URLSearchParams(window.location.search);
+var movieID = URLParams.get('id');
+
+var movieRequestURL = "http://api.themoviedb.org/3/movie/" + movieID + "?api_key=b37e4ca3ccc6c8c0a927bdcc9257084f";
+
+var movieImageBaseURL = "http://image.tmdb.org/t/p/";
+var movieImageSize = "w154/";
+
+$.getJSON({
+url: movieRequestURL,
+  success: function(result) {
+    var movieDetails = result;
+      $('.movieHeader h2').html(movieDetails.original_title);
+      $('.movieProfile img').attr('src', movieImageBaseURL + movieImageSize + movieDetails.poster_path)
+      $('.movieYear').html(new Date(movieDetails.release_date).getFullYear());
+      $('.movieRuntime span').html(movieDetails.runtime);
+      $('.movieScore span').html(movieDetails.vote_average);
+      $('.movieDesc p').html(movieDetails.overview);
+  }
+});
